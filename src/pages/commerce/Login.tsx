744b9d5e5e5d060adfb1e815bc66e2f09dbef151
hidenseek9415 <the.first9415@gmail.com>
2025-04-23 19:34:38 +0700

    fix login ui

--- src/pages/commerce/Login.tsx
+++ src/pages/commerce/Login.tsx
@@ -4,31 +4,34 @@ import { Link } from 'react-router-dom';
 
 const Login = () => {
 	return (
-		<div className='relative flex min-h-screen items-center justify-center bg-black'>
-			<div className="absolute inset-0 bg-cover bg-center bg-[url('./assets/bg.jpg')] opacity-50" />
+		<div className='relative flex min-h-screen items-center justify-center'>
+			<div className="absolute inset-0 bg-[url('./assets/bg.jpg')] bg-cover bg-center" />
 
 			<motion.div
 				initial={{ opacity: 0, y: -20 }}
 				animate={{ opacity: 1, y: 0 }}
 				transition={{ duration: 0.5 }}
-				className='relative z-10 w-full max-w-md bg-gray-900 p-6 rounded-lg shadow-lg'
+				className='z-10 w-full max-w-md rounded-lg p-6'
 			>
-				<div className='flex flex-col items-center gap-2 text-white mb-5'>
+				<div className='mb-5 flex flex-col items-center gap-2 text-white'>
 					<span className='text-3xl font-bold'>Đăng nhập InkVerse</span>
 					<span className='text-sm opacity-80'>Sử dụng Username và mật khẩu của bạn để đăng nhập</span>
 				</div>
 
-				<LoginForm />
+				<div className='relative'>
+					<LoginForm />
+					<div className='absolute left-0 top-0 z-10 h-full w-full rounded-2xl border border-black bg-black opacity-40 shadow-md' />
+				</div>
 
-				<div className='text-white text-center mt-4'>
-					<Link to='/forgot-password' className='text-blue-400 hover:underline'>
+				<div className='mt-4 text-center text-white'>
+					<Link to='/forgot-password' className='hover:underline'>
 						Quên mật khẩu?
 					</Link>
 				</div>
 
-				<div className='text-white text-center mt-2'>
+				<div className='mt-2 text-center text-white'>
 					Trở về{' '}
-					<Link to='/new-order' className='text-blue-400 hover:underline'>
+					<Link to='/new-order' className='hover:underline'>
 						Trang chủ
 					</Link>
 				</div>
